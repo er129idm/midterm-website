d62e55d2ab19941ee0b31d1cea99504554ed88fe
Effie Vision <effie.vision@gmail.com>
2020-03-10 14:37:07 -0400

    Updated JS

--- cardchoice.js
+++ cardchoice.js
@@ -2,91 +2,32 @@
 
 // from https://www.sitepoint.com/community/t/moving-an-image-using-javascript/12507
 
-document.getElementbyID("one").addEventListener("click", function(){
-  document.getElementbyID("one").style.position="absolute";
-  document.getElementbyID("one").style.top="10";
+/* NOTE:
+  Array to store every clickable card */
+var cardContainerElements = [];
+
+/* NOTE:
+  We need to wait until the page is fully loaded to attach functionality to the cards */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  /* NOTE:
+  Get every card on the page and attack the "click" listener */
+  cardContainerElements = document.getElementsByClassName("cardContainer");
+
+  console.log("Found " + cardContainerElements.length + " card containers.");
+  for (var i = 0; i < cardContainerElements.length; i++) {
+    cardContainerElements[i].addEventListener("click", function() {
+      /* NOTE:
+      event.target is the element we clicked */
+      console.log(event.target);
+      offsetCard(event.target);
+    });
+  }
 });
 
-b = document.getElementbyID("two")
-b.onclick = function(){
-b.style.top="10";}
-
-d = document.getElementbyID("three")
-d.onclick = function(){
-d.style.top="10";}
-
-e = document.getElementbyID("four")
-e.onclick = function(){
-e.style.top="10";}
-
-f = document.getElementbyID("five")
-f.onclick = function(){
-f.style.top="10";}
-
-g = document.getElementbyID("six")
-g.onclick = function(){
-g.style.top="10";}
-
-h = document.getElementbyID("seven")
-h.onclick = function(){
-h.style.top="10";}
-
-i = document.getElementbyID("eight")
-i.onclick = function(){
-i.style.top="10";}
-
-j = document.getElementbyID("nine")
-j.onclick = function(){
-j.style.top="10";}
-
-k = document.getElementbyID("ten")
-k.onclick = function(){
-k.style.top="10";}
-
-l = document.getElementbyID("eleven")
-l.onclick = function(){
-l.style.top="10";}
-
-m = document.getElementbyID("twelve")
-m.onclick = function(){
-m.style.top="10";}
-
-n = document.getElementbyID("thirteen")
-n.onclick = function(){
-n.style.top="10";}
-
-o = document.getElementbyID("fourteen")
-o.onclick = function(){
-o.style.top="10";}
-
-p = document.getElementbyID("fifteen")
-p.onclick = function(){
-p.style.top="10";}
-
-q = document.getElementbyID("sixteen")
-q.onclick = function(){
-q.style.top="10";}
-
-r = document.getElementbyID("seventeen")
-r.onclick = function(){
-r.style.top="10";}
-
-s = document.getElementbyID("eighteen")
-s.onclick = function(){
-s.style.top="10";}
-
-t = document.getElementbyID("nineteen")
-t.onclick = function(){
-t.style.top="10";}
-
-u = document.getElementbyID("twenty")
-u.onclick = function(){
-u.style.top="10";}
-
-v = document.getElementbyID("twentyone")
-v.onclick = function(){
-v.style.top="10";}
-
-w = document.getElementbyID("twentytwo")
-w.onclick = function(){
-w.style.top="10";}
+function offsetCard(cardElement) {
+  /* NOTE:
+  The "cardElement" is the div that was clicked */
+  cardElement.style.marginTop = "25px";
+}
